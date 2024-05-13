776eca1252566d40ba05667021028d99faabf3e7
kenee101 <elijah.usih@stu.cu.edu.ng>
2024-05-13 17:03:58 +0100

    InitialCommit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const http = require("http");
+const url = require("url");
 
 ///////////////////////////////////////
 // FILES
@@ -33,8 +34,61 @@ const http = require("http");
 
 /////////////////////////////////////////
 // SERVER
+const replaceTemplate = (html, product) => {
+  let output = html.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+
+  return output;
+};
+
+const overview = fs.readFileSync(
+  `${__dirname}/templates/overview.html`,
+  "utf-8"
+);
+const card = fs.readFileSync(`${__dirname}/templates/card.html`, "utf-8");
+const product = fs.readFileSync(`${__dirname}/templates/product.html`, "utf-8");
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
-  res.end("Hello from the server");
+  const pathName = req.url;
+
+  // Overview page
+  if (pathName === "/" || pathName === "/overview") {
+    res.writeHead(200, { "Content-type": "text/html" });
+
+    const cardsHtml = dataObj.map((el) => replaceTemplate(card, el)).join();
+
+    const output = overview.replace(/{%PRODUCT_CARDS%}/g, cardsHtml);
+
+    res.end(output);
+
+    // Product page
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT");
+
+    // API
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+
+    // Not found
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+    });
+    res.end("<h1>Page not found</h1>");
+  }
 });
 
 server.listen(8000, "127.0.0.1", () => {
